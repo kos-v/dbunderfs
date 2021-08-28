01fb927670f403ae7128b9cf5c045fd868a52dfe
Konstantin Voloshchuk <kosv.dev@gmail.com>
2021-08-28 10:49:53 +0600

    [feature:#12] write tests for container.Collection

---
+++ container/collection_test.go
@@ -0,0 +1,58 @@
+/*
+   Copyright 2021 The DbunderFS Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package container
+
+import "testing"
+
+func TestCollection_Append(t *testing.T) {
+	toInterfaceList := func(items []string) []interface{} {
+		interfaceItems := make([]interface{}, len(items))
+		for i, item := range items {
+			interfaceItems[i] = item
+		}
+		return interfaceItems
+	}
+
+	tests := []struct {
+		initItems     []string
+		appendItems   []string
+		expectedItems []string
+	}{
+		{[]string{}, []string{"foo", "bar", "baz"}, []string{"foo", "bar", "baz"}},
+		{[]string{"foo"}, []string{"bar", "baz"}, []string{"foo", "bar", "baz"}},
+		{[]string{"foo", "bar"}, []string{"baz"}, []string{"foo", "bar", "baz"}},
+		{[]string{"foo", "bar", "baz"}, []string{}, []string{"foo", "bar", "baz"}},
+	}
+
+	for id, test := range tests {
+		coll := Collection{list: toInterfaceList(test.initItems)}
+		for _, appendItem := range test.appendItems {
+			coll.Append(appendItem)
+		}
+
+		if len(coll.ToList()) != len(test.expectedItems) {
+			t.Errorf("Test %v fail: object contains unexpected number of items.\nExpected: %v. Result: %v.\n", id, len(test.expectedItems), len(coll.ToList()))
+			return
+		}
+
+		for i, item := range coll.ToList() {
+			if item != test.expectedItems[i] {
+				t.Errorf("Test %v fail: result data is not as expected.\nExpected: %v. Result: %v.\n", id, test.expectedItems[i], item.(string))
+			}
+		}
+	}
+}
